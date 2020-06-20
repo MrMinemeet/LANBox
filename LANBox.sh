0177d7980dbaa945ec22759ed3321dbea3666db4
MrMinemeet <alexander.voglsperger@gmail.com>
2020-06-20 20:43:14 +0200

    Setup AP & Basic website
    Setup creates an access point
    Also a basic website is setup

---
+++ LANBox.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# LANBox installer script
+# created by Alexander Voglsperger (c) 2020
+# inspired by the PirateBox which was created by Matthias Strubel
+# Project can be found on GitHub https://github.com/MrMinemeet/LANBox
+
+
+scriptfile="$(readlink -f $0)"
+CURRENT_DIR="$(dirname ${scriptfile})"
+
+clear
+echo -e '\E[0;30m'"\033[1m
+ _     _____ _   _ ____
+( )   (  _  ) ) ( )  _ \            
+| |   | (_) |  \| | (_) )  _       
+| |  _(  _  )     |  _ ( / _ \( \/ )
+| |_( ) | | | | \ | (_) ) (_) ))  ( 
+(____/(_) (_)_) (_)____/ \___/(_/\_)
+\033[0m" 
+
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+        echo "This script must be run as root"
+        exit 0
+fi
+
+# Check if packages are missing
+DEPENDENT_PACKAGES="hostapd apache2 dnsmasq python3 php net-tools"
+
+if [ "$DEPENDENT_PACKAGES" != "" ]; then
+  echo -n "Some dependencies may be missing. Would you like to install them? (Y/n): "
+  read SURE
+
+  # If user want to install missing dependencies
+  if [[ $SURE = "Y" || $SURE = "y" ]]; then
+    apt install $DEPENDENT_PACKAGES -y
+    echo "Installed dependencies"
+  fi
+fi
+
+LANBOX_CONFIG=/home/pi/Project/LANBox/conf/lanbox.conf
+if [ ! -f $LANBOX_CONFIG ] ; then
+  echo "Config-File LANBOX_CONFIG not found..."
+  exit 255
+fi
+
+#Load Config
+. $LANBOX_CONFIG
+
+# Setup AccessPoint
+$LANBOX_PATH/Setup/setup_ap.sh $LANBOX_CONFIG
---
+++ Setup/setup_ap.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Load Config
+. $1
+
+DNSMASQ_CONFIG=$CONFIG_FOLDER/dnsmasq.conf
+DNSMASQ_DEFAULT_CONFIG=$CONFIG_FOLDER/dnsmasq_default.conf
+HOSTS="$CONFIG_FOLDER/hosts"
+HOSTAPD_CONFIG="$CONFIG_FOLDER/hostapd.conf"
+HOSTS_DAEMON_CONFIG="/etc/default/hostapd"
+
+generate_hosts(){
+    hostname=$1
+    ipv4=$2
+
+    echo "$ipv4     $hostname " > $HOSTS
+}
+
+generate_dnsmasq(){
+    net=$1
+    ip_short=$2
+    lease_start=$3
+    lease_stop=$4
+    lease_time=$5
+    dnsmasq_interface=$6
+    lease_file=$7
+
+    echo "Generating dnsmasq.conf ..."
+    
+    echo "interface=$dnsmasq_interface"     > $DNSMASQ_CONFIG
+    lease_line="$net.$lease_start,$net.$lease_stop,$lease_time"
+    echo "dhcp-range=$lease_line"           >> $DNSMASQ_CONFIG
+    echo "dhcp-leasefile=$lease_file"       >> $DNSMASQ_CONFIG
+    echo "addn-hosts=$HOSTS"            >> $DNSMASQ_CONFIG
+    
+    echo "Generate link for dnsmasq"
+    rm /etc/dnsmasq.conf
+    ln -s $DNSMASQ_CONFIG /etc/dnsmasq.conf
+}
+
+generate_dnsmasq $NETWORK $IP_SHORT $DHCP_LEASE_START $DHCP_LEASE_STOP $LEASE_DURATION $INTERFACE $LEASE_LOC
+generate_hosts $HOSTNAME $IP
+
+# --- Check Config ---
+if [ -z $IP ]; then
+    echo "Please define IP in LANBOX.conf"
+    exit 1
+fi
+if [ -z $INTERFACE ]; then
+    echo "Please define interface in LANBOX.conf"
+    exit 1
+fi
+if [ -z $NETMASK ]; then
+    echo "Please define netmask in LANBOX.conf"
+    exit 1
+fi
+
+# --- Set static IP for LANBox ---
+# Bring up interface
+echo "Bringing up wifi interface $INTERFACE"
+ifconfig $INTERFACE up
+if [ $? -ne 0 ]; then
+    echo "...failed";
+    exit 1
+fi
+
+# Setting IP in interface
+echo "Setting up $INTERFACE"
+# e.g.: ifconfig wlan0 192.168.2.1 netmask 255.255.255.0
+ifconfig $INTERFACE $IP netmask $NETMASK
+if [ $? -ne 0 ]; then
+    echo "...failed";
+    exit 1
+fi
+
+# Configure hostapd
+echo "RUN_DAEMON=yes" >> $HOSTS_DAEMON_CONFIG
+echo "DAEMON_CONF=\"$HOSTAPD_CONFIG\"" >> $HOSTS_DAEMON_CONFIG
+
+# Handle services
+systemctl restart dhcpcd
+systemctl restart dnsmasq
+systemctl enable dnsmasq
+systemctl unmask hostapd
+systemctl start hostapd
+systemctl enable hostapd
+systemctl restart hostapd
